17cd0eb02919e65868374c366e90c414d714a337
Alex <alexsilva.info@gmail.com>
2022-10-26 17:11:59 -0300

    adicionado novas funcionalidades

--- containers/Login.tsx
+++ containers/Login.tsx
@@ -1,21 +1,67 @@
+import { Console } from 'console';
 import type {NextPage} from 'next';
-import { Component } from 'react';
+import { Component, useState } from 'react';
+import { executeRequest } from '../services/api';
 
-export const Login : NextPage = () =>{
+type LoginProps = {
+    setAccessToken(s:string) : void
+}
+
+export const Login : NextPage<LoginProps> = ({setAccessToken}) =>{
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const doLogin = async () => {
+        try{
+            if (!email || !password) {
+                setError('Favor preencher os campos.');
+            }
+
+            setLoading(true);
+
+            const body = {
+                login: email,
+                password
+            };
+
+            const result = await executeRequest('login', 'POST', body);
+                if (result && result.data){
+                    localStorage.setItem('accessToken', result.data.token);
+                    localStorage.setItem('name', result.data.name);
+                    localStorage.setItem('email', result.data.email);
+                    setAccessToken(result.data.token);
+                }
+        }catch(e: any){
+            console.log('Ocorreu um erro ao efetuar login', e);
+            if (e?.response?.data?.error){
+                setError(e?.response?.data?.error);
+            } else {
+                setError('Ocorreu um erro ao efetuar login, tente novamente.');
+            }
+        }
+
+        setLoading(false);
+    }
 
     return (
         <div className='container-login'>
-            <img src='/Logo FIAP.svg' alt='Logo Fiap' className='logo'/>
+            <img src='/logo.svg' alt='Logo Fiap' className='logo'/>
             <div className="form">
+                {error && <p>{error}</p>}
                 <div>
                     <img src='/mail.svg' alt='Login'/> 
-                    <input type="text" placeholder="Login" />
+                    <input type="text" placeholder="Login" 
+                        value={email} onChange={e => setEmail(e.target.value)} />
                 </div>
                 <div>
                     <img src='/lock.svg' alt='Senha'/> 
-                    <input type="password" placeholder="Senha" />
+                    <input type="password" placeholder="Senha" 
+                        value={password} onChange={e => setPassword(e.target.value)} />
                 </div>
-                <button type='button'>Login</button>
+                <button type='button' onClick={doLogin} disabled={loading}>{loading ? '...Carregando': 'Login'}</button>
             </div>
         </div>
     );
